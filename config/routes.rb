b9069088ea99ecc68484d731352bb7165ae1fdd6
marixtusimo <kdfnrjdyewhwy@yahoo.co.jp>
2021-05-25 22:46:47 +0900

    簡単ログインの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  devise_for :owners
-  devise_for :users
+  devise_for :owners, controllers: {
+    registrations: 'owners/registrations',
+    passwords: 'owners/passwords'
+  }
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   resources :posts do
     collection do
       get 'search'
@@ -9,5 +15,11 @@ Rails.application.routes.draw do
   end
   resources :rooms, only: [:show, :create] do
     resources :messages, only: :create
+  end
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+  devise_scope :owner do
+    post 'owners/guest_sign_in', to: 'owners/sessions#guest_sign_in'
   end
 end
